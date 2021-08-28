32cc4dcee8b02d7991e479e2ff469438ddda4ff0
Igor Albuquerque <43628631+igor2370@users.noreply.github.com>
2021-08-28 02:33:36 -0300

    all

--- api/index.py
+++ api/index.py
@@ -1,12 +1,19 @@
 from http.server import BaseHTTPRequestHandler
-from cowpy import cow
+from urllib import parse
 
 class handler(BaseHTTPRequestHandler):
 
-    def do_GET(self):
-        self.send_response(200)
-        self.send_header('Content-type','text/plain')
-        self.end_headers()
-        message = cow.Cowacter().milk('Hello from Python from a Serverless Function!')
-        self.wfile.write(message.encode())
-        return
+	def do_GET(self):
+		s = self.path
+		dic = dict(parse.parse_qsl(parse.urlsplit(s).query))
+		self.send_response(200)
+		self.send_header('Content-type','text/plain')
+		self.end_headers()
+
+		if "name" in dic:
+			message = "Hello, " + dic["name"] + "!"
+		else:
+			message = "Hello, stranger!"
+
+		self.wfile.write(message.encode())
+		return
